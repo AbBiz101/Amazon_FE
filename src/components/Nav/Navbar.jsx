e3c4a804bf15182b338b970aabca4e4e752a6fa7
Abnet Nega <86370531+AbBiz101@users.noreply.github.com>
2022-01-05 12:43:35 +0100

    nav resp

--- src/components/Nav/Navbar.jsx
+++ src/components/Nav/Navbar.jsx
@@ -31,97 +31,120 @@ export default function NavBar() {
 	};
 
 	return (
-			<div className="container-fluid px-0 xxxx">
-				<div className="col-12 header">
-					<div className="header_left">
-						<Link to="/">
-							<img
-								className="header_logo"
-								alt=""
-								onClick={() => dispatch(removeAProduct())}
-								src="https://pngimg.com/uploads/amazon/amazon_PNG11.png"
-							/>
-						</Link>
-						{!firstName ? (
-							<></>
-						) : (
-							<div className="d-none d-xl-block header_user_location">
-								<div>
-									<span>Deliver to {firstName}</span>
-									<h6>Stuttgart 70327‌</h6>
-								</div>
-							</div>
-						)}
-						{role === 'ADMIN' ? (
-							<div className="d-none d-md-inline mt-3 header_back_office">
-								<h6 onClick={(e) => history('/backOffice')}>Back Office</h6>
-							</div>
-						) : (
-							<></>
-						)}
-					</div>
-
-					<div className="header_search">
-						<DropdownButton
-							className="category_list"
-							as="ButtonGroup"
-							title="All"
-						>
-							<Dropdown.Item eventKey="1">Action</Dropdown.Item>
-							<Dropdown.Item eventKey="2">Another action</Dropdown.Item>
-							<Dropdown.Item eventKey="3" active>
-								Active Item
-							</Dropdown.Item>
-						</DropdownButton>
-
-						<input
-							onChange={(e) => dispatch(searchProducts(e.target.value))}
-							type="text"
-							className="header_search_input"
+		<div className="container-fluid px-0 d-flex xxxx">
+			<div className="col-12 header">
+				<div className="header_left">
+					<Link to="/">
+						<img
+							className="d-sm-block d-none header_logo"
+							alt=""
+							onClick={() => dispatch(removeAProduct())}
+							src="https://pngimg.com/uploads/amazon/amazon_PNG11.png"
 						/>
-						<BsSearch size={20} className="header_search_icon" />
-					</div>
-
-					{firstName ? (
-						<div className="mt-2 header_options">
-							<div className="d-none d-lg-block header_options_1">
-								<span className="d-inline">Returns &</span>
-								<h5>Orders</h5>
-							</div>
-
-							<div className="d-none d-lg-block header_options_2">
-								<Link to="/checkout">
-									<h4 className="mt-1">
-										<RiShoppingCartLine size={35} />
-										{cartLength}
-									</h4>
-								</Link>
-							</div>
-
-							<div className="userbutn">
-								<SplitButton
-									className="header_options_3"
-									key="left"
-									drop="left"
-									title={firstName}
-								>
-									<Dropdown.Item onClick={() => history('/me')}>
-										My profile
-									</Dropdown.Item>
-									<Dropdown.Item onClick={(e) => LOGOUT()}>
-										Logout
-									</Dropdown.Item>
-								</SplitButton>
+					</Link>
+					{!firstName ? (
+						<></>
+					) : (
+						<div className="d-none d-xl-block header_user_location">
+							<div>
+								<span>Deliver to {firstName}</span>
+								<h6>Stuttgart 70327‌</h6>
 							</div>
 						</div>
+					)}
+					{role === 'ADMIN' ? (
+						<div className="d-none d-md-inline mt-3 header_back_office">
+							<h6 onClick={(e) => history('/backOffice')}>Back Office</h6>
+						</div>
 					) : (
-						<Link to="login">
-							<Button type="submit" className="mt-2 bbttnn">
-								Login
-							</Button>
-						</Link>
+						<></>
 					)}
 				</div>
+
+				<div className="header_search">
+					<DropdownButton
+						className="category_list"
+						as="ButtonGroup"
+						title="All"
+					>
+						<Dropdown.Item eventKey="1">Action</Dropdown.Item>
+						<Dropdown.Item eventKey="2">Another action</Dropdown.Item>
+						<Dropdown.Item eventKey="3">Active Item</Dropdown.Item>
+					</DropdownButton>
+
+					<input
+						onChange={(e) => dispatch(searchProducts(e.target.value))}
+						type="text"
+						className="header_search_input"
+					/>
+					<BsSearch size={20} className="header_search_icon" />
+				</div>
+
+				{firstName ? (
+					<div className="mt-2 header_options">
+						<div className="d-none d-lg-block header_options_1">
+							<span className="d-inline">Returns &</span>
+							<h5>Orders</h5>
+						</div>
+
+						<div className="d-none d-lg-block header_options_2">
+							<Link to="/checkout">
+								<h4 className="mt-1">
+									<RiShoppingCartLine size={35} />
+									{cartLength}
+								</h4>
+							</Link>
+						</div>
+
+						<div className="userbutn">
+							<SplitButton
+								className="header_options_3"
+								key="left"
+								drop="left"
+								title={firstName}
+							>
+								<Dropdown.Item
+									className="d-block d-sm-none"
+									onClick={() => history('/')}
+								>
+									Home
+								</Dropdown.Item>
+
+								<Dropdown.Item
+									className="d-block d-lg-none"
+									onClick={() => history('/checkout')}
+								>
+									<RiShoppingCartLine size={25} />
+									<span> {cartLength}</span>
+								</Dropdown.Item>
+								<Dropdown.Item onClick={() => history('/me')}>
+									My profile
+								</Dropdown.Item>
+								{role === 'ADMIN' ? (
+									<Dropdown.Item
+										className="d-block d-md-none"
+										onClick={(e) => history('/backOffice')}
+									>
+										Back Office
+									</Dropdown.Item>
+								) : (
+									<></>
+								)}
+								<Dropdown.Item className="d-block d-lg-none">
+									Returns & Orders
+								</Dropdown.Item>
+								<Dropdown.Item onClick={(e) => LOGOUT()}>Logout</Dropdown.Item>
+							</SplitButton>
+						</div>
+					</div>
+				) : (
+					<Link to="login">
+						<Button type="submit" className="mt-2 bbttnn">
+							Login
+						</Button>
+					</Link>
+				)}
 			</div>
+		</div>
 	);
 }
