34b0cb439774dd6dcc23009294e28d175d602848
Abnet Nega <86370531+AbBiz101@users.noreply.github.com>
2022-01-05 10:45:19 +0100

    css update

--- src/components/ProductCompTwo/ProductCompTwo.jsx
+++ src/components/ProductCompTwo/ProductCompTwo.jsx
@@ -1,7 +1,7 @@
 import './ProductCompTwo.css';
 export default function ProductCompTwo() {
 	return (
-		<div className="col-md-6 px-0 mx-0  product_comp_two">
+		<div className="col-md-6 d-none d-sm-block px-0 mx-0  product_comp_two">
 			<h4 className="product_name">Video: Recommended for you</h4>
 			<h5 className="product_name">The Voyeurs</h5>
 			<img
